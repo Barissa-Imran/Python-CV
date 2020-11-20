1e1befed1a39681fd36af8845f7fa7d3e23ef007
Barissa-Imran <imranbarissa@gmail.com>
2020-11-20 04:48:50 +0300

    Added Text to speech functionality
    used the pyttse3 library

--- python-CV.py
+++ python-CV.py
@@ -1,5 +1,9 @@
 from docx import Document
 from docx.shared import Inches
+import pyttsx3 
+
+def speak(text):
+    pyttsx3.speak(text)
 
 document = Document()
 
@@ -10,8 +14,14 @@ document.add_picture(
 )
 
 # name phone number and email details
+speak('what is your name? ')
 name = input('what is your name? ')
+speak('Hello' + name +'How are you today? ')
+
+speak('what is your phone number? ')
 phone_number = input('what is your phone number? ')
+
+speak('please provide an email address')
 email = input('what is your email? ')
 
 document.add_paragraph(
@@ -19,6 +29,7 @@ document.add_paragraph(
 )
 # about me
 document.add_heading('About me')
+speak('Tell me about yourself?')
 document.add_paragraph(
     input('Tell me about yourself? ')
 )
@@ -27,8 +38,10 @@ document.add_paragraph(
 document.add_heading('Work Experience')
 p = document.add_paragraph()
 
+speak('This is the work experience section')
+speak('Please read the prompts on the screen and answer appropiately')
 company = input('Enter Company ')
-from_date = input('Fro date ')
+from_date = input('From date ')
 to_date = input('To Date')
 
 p.add_run(company + ' ').bold = True
